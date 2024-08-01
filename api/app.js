9f8482b70a540fea21807324fb8c378fb9187f98
Madhawa Rathnayake <95226002+MadhawaRathnayake@users.noreply.github.com>
2024-08-01 11:30:46 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -13,7 +13,7 @@ const __dirname = path.resolve();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin:"https://easycar.onrender.com", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
